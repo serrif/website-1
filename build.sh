d6131e24ae318db6041925bc1afc968234b7d431
mathias <mail@mathias.is>
2018-04-24 17:33:19 +0000

    Push latest redesign

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 cd $HOME/build-websites/kaboom-website/
 rm Gemfile.lock
-bundle install
-if jekyll build --destination $HOME/html/kaboom.pw/; then
+#bundle install
+jekyll build --destination $HOME/html/kaboom.pw/
+#if jekyll build --destination $HOME/html/kaboom.pw/; then
 	cd $HOME/html/kaboom.pw/
-	rm -rf *.br
-	rm -rf *.gz
-	for file in `find . -type f -name '*'`; do
-		$HOME/build-websites/zopfli $file
-		$HOME/build-websites/brotli $file
-	done
-fi
+	#rm -rf *.br
+	#rm -rf *.gz
+	#for file in `find . -type f -name '*'`; do
+	#	$HOME/build-websites/zopfli $file
+	#	$HOME/build-websites/brotli $file
+	#done
+#fi
